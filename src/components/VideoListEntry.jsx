224869d2eede9bf444526bccdab988c857f33e1e
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-19 21:33:15 -0700

    Correct usage of example videos

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,17 +1,16 @@
 var VideoListEntry = (props) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div>{props.video.title}</div>
-      <div>{props.video.description}</div>
+      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div className="video-list-entry-detail">{props.video.snippet.description}</div>
     </div>
   </div>
 );
 
-// </div><div className="video-list-entry-title">{props.snippet.title}</div>
-// <div className="video-list-entry-detail">{props.snippet.description}</div>
+// "https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg"
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
